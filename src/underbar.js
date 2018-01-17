7287155004d488cca9c58cd7ed0264b376c6178b
Busyreadingsomething <oro1919@gmail.com>
2018-01-16 19:12:13 -0800

    Finished part 2 for Underbar

--- src/underbar.js
+++ src/underbar.js
@@ -357,9 +357,7 @@
       if (!obj.hasOwnProperty(stringifiedArgs)) {
         obj[stringifiedArgs] = func.apply(this, args);
       }
-      console.log(stringifiedArgs);
       return obj[stringifiedArgs];
-
     };
   };
 
@@ -370,6 +368,10 @@
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
   _.delay = function(func, wait) {
+    var args = Array.prototype.slice.call(arguments, 2);
+    setTimeout(function() {
+      return func.apply(this, args);
+    }, wait);
   };
 
 
